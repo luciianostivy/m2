17bcc006b1fb876a783166e60119591c41e7cb7f
luciianostivy <lucianostivy@gmail.com>
2022-05-23 16:04:44 -0300

    cambio

--- 11-Redux/hw-js/src/index.js
+++ 11-Redux/hw-js/src/index.js
@@ -6,25 +6,27 @@ const { incremento, decremento } = require('./actions');
 var store;
 
 // Obtenemos el elemento con el id `valor`.
-var valor;
+var valor = document.getElementById('valor');
 
 // Esta funcion nos va a servir para actualizar nuestro DOM con el valor que tengamos en nuestro Store.
 // En el primer render y cada vez que nos subscribamos al Store.
 // Utilizamos el elemento obtenido arriba para mostrar el State.
 function renderContador() {
   // Obtenemos la propiedad 'contador' de nuestro store:
-  
+  valor.innerText = store.getState().contador; 
   // Seteamos el numero obtenido como texto dentro del elemento con id 'valor':
 
 }
 
 // Ejecutamos la funcion 'renderContador':
-
+renderContador();
 
 
 // Nos subscribimos al store pasandole la misma funcion. Asi cada vez que llegue una accion, ejecutamos la funcion:
-
+store.subscribe(renderContador);
 
 
 // Por ultimo, utilizamos los botones de nuestro HTML para que cada vez que hagamos click,
 // hagan un dispatch al store de la accion correspondiente:
+document.getElementById("incremento").onclick = () => store.dispatch(incremento());
+document.getElementById("decremento").onclick = () => store.dispatch(decremento());
