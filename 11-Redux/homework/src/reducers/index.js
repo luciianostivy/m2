59e74e89437fa7a46fe657c2d58469187ba2d689
luciianostivy <lucianostivy@gmail.com>
2022-05-23 16:05:36 -0300

    cambio

--- 11-Redux/homework/src/reducers/index.js
+++ 11-Redux/homework/src/reducers/index.js
@@ -11,15 +11,13 @@ export default (state = initialState, action) => {
   switch (action.type) {
     case INCREMENT:
     // completa para este caso
-      return {
-        ...state, contador: state.contador + 1
-      }
+      return{...state, count : state.count +1};
+     
     case DECREMENT:
     // Fill para este otro
-      return {
-        ...state, contador: state.contador - 1
-      }
+     return {...state, count: state.count -1};
+    
     default:
-      return {...state};
+      return state;
   }
 };
